865aa95bb42d57ec14d7b1e910e8d8a041a62460
Danielle De La Pascua <58483225+drdelapascua@users.noreply.github.com>
2021-04-01 15:37:15 -0700

    Created data folders and first script
    Data folders contain datasets for labs in the course, the first script pulls up data from clams dataset

---
+++ lab-1.R
@@ -0,0 +1,13 @@
+# EVE 298 - Linear mixed modelins in Ecology and Evolution 
+# Danielle De La Pascua
+
+###
+### Lab 1 ----
+###
+
+ 'use three dots in files page to navigate to course files'
+
+setwd("~/GitHub/linear-mized-modeling-eve298")
+
+clams <- read.table("Datasets/Clams.txt")
+
